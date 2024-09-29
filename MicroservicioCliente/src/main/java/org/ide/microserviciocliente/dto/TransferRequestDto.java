19b88bf7042e4ded878be1369fcb6c5719884119
Jorge <jorge.quenta@utec.edu.pe>
2024-09-29 17:21:16 -0500

    commit

--- MicroservicioCliente/src/main/java/org/ide/microserviciocliente/dto/TransferRequestDto.java
+++ MicroservicioCliente/src/main/java/org/ide/microserviciocliente/dto/TransferRequestDto.java
@@ -1,8 +1,13 @@
 package org.ide.microserviciocliente.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class TransferRequestDto {
+
+    @JsonProperty("destinatario_id")
     private Long destinatarioId;
+
     private Double monto;
 }
